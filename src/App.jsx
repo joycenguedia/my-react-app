45585d34ea9e43f05bca2bc57283506205b29b01
joycenguedia <gracenguedia@gmail.com>
2023-06-06 17:37:01 +0100

    video

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Navbar from './components/navbar/Navbar'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
-import Portfolio from './components/portfolio/Portfolio'
-import Testimonial from './components/testimonial/Testimonial'
+//import Portfolio from './components/portfolio/Portfolio'
+//import Testimonial from './components/testimonial/Testimonial'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -18,8 +18,8 @@ function App() {
             <About/>
             <Experience/>
             <Services/>
-            {/* <Portfolio/> */}
-            {/* <Testimonial/> */}
+            {/* <Portfolio/>  */}
+            {/* <Testimonial/>  */}
             <Contact/>
             <Footer/>
         </>
